f2f29487e2bb130f076ec83f9e3d750ce6ded289
Alejandro Gomez <alejodiscovery20@gmail.com>
2023-07-28 19:35:03 -0500

    chore: Added gitignore

--- ArduinoBuilder/src/build-uno/userlibs/ZumoShield/PololuBuzzer.d
+++
@@ -1,3 +0,0 @@
-build-uno/userlibs//ZumoShield/PololuBuzzer.o: \
- /arduinoLibraries///ZumoShield/PololuBuzzer.cpp \
- /arduinoLibraries///ZumoShield/PololuBuzzer.h
--- RPi/VideoStreaming/catkin_ws/build/video_streamming/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /catkin_ws/src/video_streamming
-# Build directory: /catkin_ws/build/video_streamming
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
